c33f3c3f73a8e55a5fea312b2341f2197a6ee31c
sirMahdiRahmani <sir.mahdirahmani@gmail.com>
2024-04-27 02:03:20 +0330

    Pointers And Refrences

--- CppTutorial/src/Main.cpp
+++ CppTutorial/src/Main.cpp
@@ -1,6 +1,19 @@
+#include <iostream>
 #include "Log.h"
+
+using namespace std;
 
 int main()
 {
-	Log("heelo");
+	int a = 8;
+	int b = 5;
+
+	int* ptr = &a;
+	*ptr = 2;
+
+	ptr = &b;
+	*ptr = 45;
+
+	cout << a << endl;
+	cout << b << endl;
 }
