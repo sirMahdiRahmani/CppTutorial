c0cb73ee70ff5857a320eacd0137facd4f619eb7
sirMahdiRahmani <sir.mahdirahmani@gmail.com>
2024-05-12 09:42:54 +0330

    Create Log Class

--- CppTutorial/src/Main.cpp
+++ CppTutorial/src/Main.cpp
@@ -1,19 +1,53 @@
 #include <iostream>
-#include "Log.h"
 
 using namespace std;
 
+class Log
+{
+public:
+	int LogLevelInfo = 2;
+	int LogLevelWarning = 1;
+	int LogLevelError = 0;
+
+private:
+	int m_LogLevel = LogLevelInfo;
+
+public:
+	void SetLevel(int level)
+	{
+		m_LogLevel = level;
+	}
+
+	void Info(const char* message)
+	{
+		if (m_LogLevel >= LogLevelInfo)
+		{
+			cout << "[Info]: " << message << endl;
+		}
+	}
+
+	void Warn(const char* message)
+	{
+		if (m_LogLevel >= LogLevelWarning)
+		{
+			cout << "[Warning]: " << message << endl;
+		}
+	}
+
+	void Error(const char* message)
+	{
+		if (m_LogLevel >= LogLevelError)
+		{
+			cout << "[Error]: " << message << endl;
+		}
+	}
+};
+
 int main()
 {
-	int a = 8;
-	int b = 5;
-
-	int* ptr = &a;
-	*ptr = 2;
-
-	ptr = &b;
-	*ptr = 45;
-
-	cout << a << endl;
-	cout << b << endl;
+	Log log;
+	log.SetLevel(log.LogLevelWarning);
+	log.Info("Hello Info!");
+	log.Warn("Hello Warning!");
+	log.Error("Hello Error!");
 }
