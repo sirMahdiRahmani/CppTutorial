cea366d07c5d72c59b50d0da5a56e04f07235589
sirMahdiRahmani <sir.mahdirahmani@gmail.com>
2024-05-23 18:46:06 +0330

    Interface

--- CppTutorial/src/Log.cpp
+++ CppTutorial/src/Log.cpp
@@ -2,7 +2,51 @@
 
 using namespace std;
 
+
 void Log(const char* message)
 {
 	cout << message << endl;
 }
+
+
+class Log
+{
+public:
+	enum
+	{
+		LevelError = 0, LevelWarning, LevelInfo
+	};
+
+private:
+	int m_LogLevel = LevelInfo;
+
+public:
+	void SetLevel(int level)
+	{
+		m_LogLevel = level;
+	}
+
+	void Info(const char* message)
+	{
+		if (m_LogLevel >= LevelInfo)
+		{
+			cout << "[Info]: " << message << endl;
+		}
+	}
+
+	void Warn(const char* message)
+	{
+		if (m_LogLevel >= LevelWarning)
+		{
+			cout << "[Warning]: " << message << endl;
+		}
+	}
+
+	void Error(const char* message)
+	{
+		if (m_LogLevel >= LevelError)
+		{
+			cout << "[Error]: " << message << endl;
+		}
+	}
+};
--- CppTutorial/src/Main.cpp
+++ CppTutorial/src/Main.cpp
@@ -2,52 +2,33 @@
 
 using namespace std;
 
-class Log
+
+class Actor
 {
 public:
-	int LogLevelInfo = 2;
-	int LogLevelWarning = 1;
-	int LogLevelError = 0;
+	int X;
+	int Y;
+	int Z;
 
-private:
-	int m_LogLevel = LogLevelInfo;
-
-public:
-	void SetLevel(int level)
+	virtual void Move(int x, int y, int z)
 	{
-		m_LogLevel = level;
+		X += x;
+		Y += y;
+		Z += z;
 	}
+};
 
-	void Info(const char* message)
+class Vehicle : public Actor
+{
+	int Speed;
+	void Move(int x, int y, int z) override
 	{
-		if (m_LogLevel >= LogLevelInfo)
-		{
-			cout << "[Info]: " << message << endl;
-		}
-	}
-
-	void Warn(const char* message)
-	{
-		if (m_LogLevel >= LogLevelWarning)
-		{
-			cout << "[Warning]: " << message << endl;
-		}
-	}
-
-	void Error(const char* message)
-	{
-		if (m_LogLevel >= LogLevelError)
-		{
-			cout << "[Error]: " << message << endl;
-		}
+		X += x * Speed;
+		Y += y * Speed;
+		Z += z * Speed;
 	}
 };
 
 int main()
 {
-	Log log;
-	log.SetLevel(log.LogLevelWarning);
-	log.Info("Hello Info!");
-	log.Warn("Hello Warning!");
-	log.Error("Hello Error!");
 }
