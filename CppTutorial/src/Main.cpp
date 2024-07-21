f57310a16923d91fb7d176f2ba13ef2b5d9cfe61
sirMahdiRahmani <sir.mahdirahmani@gmail.com>
2024-07-21 19:37:46 +0330

    Measure

--- CppTutorial/src/Main.cpp
+++ CppTutorial/src/Main.cpp
@@ -5,37 +5,24 @@ struct Vector2
 	float x, y;
 };
 
+
+
 int main()
 {
+	std::vector<int*> SharedPtrs;
+	SharedPtrs.reserve(1000000000);
 	{
 		std::cout << "Shared Pointers\n";
-		std::array<std::shared_ptr<Vector2>, 1000> SharedPtrs;
-		Timer timer;
-		for (int i = 0; i < SharedPtrs.size(); i++)
-		{
-			SharedPtrs[i] = std::make_shared<Vector2>();
-		}
-	}
-
-	{
-		std::cout << "Shared Pointers with New\n";
-		std::array<std::shared_ptr<Vector2>, 1000> SharedPtrs2;
 		
-		Timer timer; // start to measure from here
-		for (int i = 0; i < SharedPtrs2.size(); i++)
-		{
-			SharedPtrs2[i] = std::shared_ptr<Vector2>(new Vector2());
-		} // destruct the timer and calc the taken time
-	}
-
-	{
-		std::cout << "Unique Pointers\n";
-
-		std::array<std::unique_ptr<Vector2>, 1000> UniquePtrs;
 		Timer timer;
-		for (int i = 0; i < UniquePtrs.size(); i++)
+		for (int i = 0; i < 1000000000; i++)
 		{
-			UniquePtrs[i] = std::make_unique<Vector2>();
+			SharedPtrs[i] = new int(i);
 		}
 	}
+	std::cout << SharedPtrs[23000] << std::endl;
+	/*for (int i = 0; i < SharedPtrs.size(); i++)
+	{
+		delete SharedPtrs[i];
+	}*/
 }
