3e19d133bda5ab6fce6b464d28710eae9b01873f
sirMahdiRahmani <sir.mahdirahmani@gmail.com>
2024-08-19 12:00:31 +0330

    PROFILING Micro

--- CppTutorial/src/Main.cpp
+++ CppTutorial/src/Main.cpp
@@ -1,8 +1,22 @@
 #include "pch.h"
 
+#define PROFILING 1
+
+#if PROFILING
+
+#define PROFILE_SCOPE(name) InstrumentationTimer timer ##__LINE__(name)
+#define PROFILE_FUNCTION() PROFILE_SCOPE(__FUNCTION__)
+
+#else
+
+#define PROFILE_SCOPE(name)
+#define PROFILE_FUNCTION() PROFILE_SCOPE(__FUNCTION__)
+
+#endif
+
 void Function() 
 {
-	InstrumentationTimer timer("F");
+	PROFILE_FUNCTION();
 
 	for (int i = 0; i < 100; i++) 
 	{
