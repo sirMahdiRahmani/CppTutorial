576bb1922f68b81a6982976c8258d28447f8fde2
sirMahdiRahmani <sir.mahdirahmani@gmail.com>
2024-08-19 12:11:08 +0330

    Visual Bench marking

--- CppTutorial/src/Main.cpp
+++ CppTutorial/src/Main.cpp
@@ -14,23 +14,37 @@
 
 #endif
 
-void Function() 
+void Function1() 
 {
 	PROFILE_FUNCTION();
 
-	for (int i = 0; i < 100; i++) 
+	for (int i = 0; i < 10000; i++) 
+	{
+		std::cout << "Hello\n";
+	}
+}
+void Function2() 
+{
+	PROFILE_FUNCTION();
+
+	for (int i = 0; i < 10000; i++) 
 	{
 		std::cout << "Hello\n";
 	}
 }
 
+void Bench()
+{
+	PROFILE_FUNCTION();
+	std::cout << "Run Bench\n";
+	Function1();
+	Function2();
+}
+
 int main() 
 {	
 	Instrumentor::Get().BeginSession("Main");
-	Function();
+	Bench();
 	Instrumentor::Get().EndSession();
-
-	
-
 }
 
