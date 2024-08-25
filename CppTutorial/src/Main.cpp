7f51b953939b6543399eb746fc1eb8173f2543af
sirMahdiRahmani <sir.mahdirahmani@gmail.com>
2024-08-25 17:01:59 +0330

    AllocationMetrics

--- CppTutorial/src/Main.cpp
+++ CppTutorial/src/Main.cpp
@@ -1,28 +1,43 @@
 #include "pch.h"
 
-
-class Random
+struct AllocationMetrics // struct for allocation metrics with a method to calculate
 {
-public:
-	static Random& Get()
-	{
-		static Random s_Instance;
-		return s_Instance;
-	}
+	uint32_t TotalAllocated = 0;
+	uint32_t TotalFreed = 0;
 
-	float Float()
-	{
-		return RandomNumber;
-	}
-private:
-	float RandomNumber = 0.4f;
-	Random() {};
+	uint32_t CurrentUsage() { return TotalAllocated - TotalFreed; }
 };
+
+static AllocationMetrics s_AllocationMetrics; // create static instance of Allocation Metrics
+
+void* operator new(size_t size) 
+{
+	s_AllocationMetrics.TotalAllocated += size; // add value to allocation Metrics Total Allocated
+	std::cout << "Allocating " << size << " Bytes of Memory" << std::endl;
+	return malloc(size);
+}
+
+void operator delete (void* memory, size_t size)
+{
+	s_AllocationMetrics.TotalFreed += size; // add value to allocation Metrics Total Freed
+	std::cout << "Freeing " << size << " Bytes of Memory" << std::endl;
+	return free(memory);
+}
+
+void PrintMemoryUsage() // Create a Function to call for getting memory usage
+{
+	std::cout << "Memory Usage: " << s_AllocationMetrics.CurrentUsage() << " Bytes\n";
+}
+
 
 int main()
 {
-	Instrumentor::Get().BeginSession("Main"); // Start Instumentor to create file and start time
-	Random& random = Random::Get();
-	std::cout << random.Float();
-	Instrumentor::Get().EndSession();         // Stop Instumentor to Ending file and stop time
+	{
+		PrintMemoryUsage();
+		std::unique_ptr<std::string> nameUnq = std::make_unique<std::string>("Mahdi");
+		PrintMemoryUsage();
+	}
+	PrintMemoryUsage();
+	std::string name = "Mahdi";
+	PrintMemoryUsage();
 }
