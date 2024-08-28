943efa837b420f54c9f555fb07cc88182159a072
sirMahdiRahmani <sir.mahdirahmani@gmail.com>
2024-08-28 15:33:49 +0330

    Iterators

--- CppTutorial/src/Main.cpp
+++ CppTutorial/src/Main.cpp
@@ -1,62 +1,53 @@
 #include "pch.h"
-
-
-struct Vector3
-{
-	float x = 0.0f, y = 0.0f, z = 0.0f;
-
-	Vector3() {}
-	Vector3(float scalar)
-		: x(scalar), y(scalar), z(scalar) {}
-	Vector3(float X, float Y, float Z)
-		: x(X), y(Y), z(Z) {}
-
-	Vector3(const Vector3& other)
-		: x(other.x), y(other.y), z(other.z)
-	{
-		std::cout << "Copy Vector3 ----------\n";
-	}
-
-	Vector3(Vector3&& other)
-		: x(other.x), y(other.y), z(other.z)
-	{
-		std::cout << "Move Vector3 ----------\n";
-	}
-	~Vector3()
-	{
-		std::cout << "Destroy Vector3 ----------\n";
-	}
-
-	Vector3& operator=(const Vector3& other)
-	{
-		std::cout << "Copied\n";
-		x = other.x;
-		y = other.y;
-		z = other.z;
-		return *this;
-	}
-
-	Vector3& operator=(Vector3&& other)
-	{
-		std::cout << "Moved\n";
-		x = other.x;
-		y = other.y;
-		z = other.z;
-		return *this;
-	}
-};
+#include <unordered_map>
 
 int main()
 {
-	{
-		Vector<Vector3> vector;
-		vector.EmplaceBack(1.2f);
-		vector.PushBack(Vector3(2.0f, 3.4f, 7.0f));
-		vector.PushBack(Vector3());
+	std::vector<int> values = { 1, 2, 5, 7, 1, 7, 1, 55 };
 
-		PrintVector(vector);
-		PrintMemoryUsage();
-		vector.Clear();
+	for (int i = 0; i < values.size(); i++)
+	{
+		std::cout << values[i] << std::endl;
 	}
-	PrintMemoryUsage();
+
+	std::cout << std::endl;
+
+	for (int value : values)
+	{
+		std::cout << value << std::endl;
+	}
+
+	std::cout << std::endl;
+
+	for (std::vector<int>::iterator it = values.begin();
+		it != values.end(); it++)
+	{
+		std::cout << *it << std::endl;
+	}
+
+	// -------------------------------------------------
+	std::unordered_map<std::string, int> map;
+	map["C#"] = 8;
+	map["C++"] = 10;
+
+	for (std::unordered_map<std::string, int>::const_iterator it = map.begin(); it != map.end(); it++)
+	{
+		auto& key = it->first;
+		auto& value = it->second;
+
+		std::cout << key << " = " << value << std::endl;
+	}
+
+	for (auto kv : map)
+	{
+		auto& key = kv.first;
+		auto& value = kv.second;
+
+		std::cout << key << " = " << value << std::endl;
+	}
+
+	for (auto [key, value] : map)
+	{
+		std::cout << key << " = " << value << std::endl;
+	}
 }
